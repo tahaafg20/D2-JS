dca67c17bf52561ba78a046dc7fbd5fd23591b4d
Taha <baktash.29.ra@gmail.com>
2019-07-18 18:25:17 +0800

    added new exercises

--- D2_DOM_exercises/04-Recode-Racer/lib/recode_racer.js
+++ D2_DOM_exercises/04-Recode-Racer/lib/recode_racer.js
@@ -4,13 +4,22 @@ document.addEventListener("keyup", event => {
         // var playerA = document.querySelector("#player1_race")
         var currentPlaceA = document.querySelector("#player1_race td.active");
         var nextPlaceA = currentPlaceA.nextElementSibling
+        if (currentPlaceA.classList.contains("finish")){
+            alert("PLAYER A WON(RED BUS)")
+        } else {
         currentPlaceA.classList.remove("active");
         nextPlaceA.classList.add("active");
+        }
+
     } else if (event.keyCode === 81) {
         // var playerB = document.querySelector("#player2_race")
         var currentPlaceB = document.querySelector("#player2_race td.active");
         var nextPlaceB = currentPlaceB.nextElementSibling
+        if (currentPlaceB.classList.contains("finish")){
+            alert("PLAYER B WON(BLUE BUS)")
+        } else {
         currentPlaceB.classList.remove("active");
         nextPlaceB.classList.add("active");
+        }
     }
 });
