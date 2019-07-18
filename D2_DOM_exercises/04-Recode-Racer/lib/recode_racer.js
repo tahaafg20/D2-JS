fac9d4eb4c42d3a2e4a292f3705da02bf19b242d
Taha <baktash.29.ra@gmail.com>
2019-07-18 20:13:39 +0800

    finished the exercise 4 recode racer

--- D2_DOM_exercises/04-Recode-Racer/lib/recode_racer.js
+++ D2_DOM_exercises/04-Recode-Racer/lib/recode_racer.js
@@ -1,11 +1,19 @@
 // TODO: write your code here
 document.addEventListener("keyup", event => {
+    var firstPlace = document.querySelectorAll("tr td:first-child");
     if (event.keyCode === 80) {
         // var playerA = document.querySelector("#player1_race")
         var currentPlaceA = document.querySelector("#player1_race td.active");
-        var nextPlaceA = currentPlaceA.nextElementSibling
+        var nextPlaceA = currentPlaceA.nextElementSibling;
         if (currentPlaceA.classList.contains("finish")){
-            alert("PLAYER A WON(RED BUS)")
+            var finalB = document.querySelector("#player2_race td.active");
+            currentPlaceA.classList.remove("active");
+            finalB.classList.remove("active");
+            alert("PLAYER A WON! (RED BUS)");
+            firstPlace.forEach(function (playerFirstPlace){
+                playerFirstPlace.classList.add("active");
+                // currentPlaceB.classList.remove("active")
+            });
         } else {
         currentPlaceA.classList.remove("active");
         nextPlaceA.classList.add("active");
@@ -16,7 +24,14 @@ document.addEventListener("keyup", event => {
         var currentPlaceB = document.querySelector("#player2_race td.active");
         var nextPlaceB = currentPlaceB.nextElementSibling
         if (currentPlaceB.classList.contains("finish")){
-            alert("PLAYER B WON(BLUE BUS)")
+            var finalA = document.querySelector("#player1_race td.active");
+            currentPlaceB.classList.remove("active");
+            alert("PLAYER B WON! (BLUE BUS)");
+            finalA.classList.remove("active");
+            firstPlace.forEach(function (playerFirstPlace){
+                // currentPlaceA.classList.remove("active")
+                playerFirstPlace.classList.add("active");
+            });
         } else {
         currentPlaceB.classList.remove("active");
         nextPlaceB.classList.add("active");
