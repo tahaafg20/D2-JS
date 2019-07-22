f0da77020053678c1f526d7baec1957cd4a8e92a
Taha <baktash.29.ra@gmail.com>
2019-07-22 08:06:08 +0800

    Added the files

--- D2_DOM_exercises/05-Fake-Inbox/lib/inbox.js
+++ D2_DOM_exercises/05-Fake-Inbox/lib/inbox.js
@@ -2,19 +2,65 @@
 
 const hasNewMessage = () => {
   // TODO: return true with a probability of 20%.
+    return Math.random() < 0.2;
 };
 
 const newMessage = () => {
   // TODO: return a random message as an object with two keys, subject and sender
+  senderArray = ["Selena Gomez", "James Bond", "C.Ronaldo", "Salman Khan", "Ibrahimovich"];
+  subjectArray= ["Please come to my concert", "I played in a new movie, let's watch it together", "Dont you want to train me?", "I fell in love with you let's have a date", "Man I have a problem need your consultant"];
+  var emailSender = senderArray[Math.floor(Math.random()*senderArray.length)];
+  var emailSubject = subjectArray[Math.floor(Math.random()*subjectArray.length)];
+  return {
+    sender: emailSender,
+    subject: emailSubject
+  };
 };
 
 const appendMessageToDom = (message) => {
   // TODO: append the given message to the DOM (as a new row of `#inbox`)
+  var senderDiv = document.createElement("div");
+  var messageDiv = document.createElement("div");
+  var messageConditionDiv = document.createElement("div");
+  var messageDetails = Object.values(message);
+  var boldSenderText = document.createElement("H1");
+  var boldSubjectText = document.createElement("H1");
+  // boldSenderText.innerHTML = `<h1>${messageDetails[0]}</h1>`;
+  // boldSubjectText.innerHTML = `<h1>${messageDetails[1]}</h1>`;
+  // senderDiv.appendChild(boldSenderText);
+  // messageDiv.appendChild(boldSubjectText);
+  // senderDiv.innerHTML = `<h1>${messageDetails[0]}</h1>`;
+  // messageDiv.innerHTML = `<h1>${messageDetails[1]}</h1>`;
+  var senderText = document.createTextNode(`${message["sender"]}`);
+  var subjectText = document.createTextNode(`${message["subject"]}`);
+  // senderDiv.appendChild(senderText);
+  // messageDiv.appendChild(subjectText);
+  senderDiv.innerHTML = `${senderText}`;
+  messageDiv.innerHTML = `${subjectText}`;
+  messageConditionDiv.appendChild(senderDiv);
+  messageConditionDiv.appendChild(messageDiv);
+  messageConditionDiv.classList.add("row");
+  messageConditionDiv.classList.add("message");
+  messageConditionDiv.classList.add("unread");
+  var inboxRow = document.querySelector("#inbox");
+  inboxRow.insertBefore(messageConditionDiv, inboxRow.childNodes[0]);
+  senderDiv.classList.add("col-3");
+  messageDiv.classList.add("col-9");
+  senderDiv.innerHTML = messageDetails[0];
+  messageDiv.innerHTML = messageDetails[1];
 };
 
 const refresh = () => {
   // TODO: Implement the global refresh logic. If there is a new message,
   //       append it to the DOM. Update the unread counter in title as well.
+  if (hasNewMessage()){
+    appendMessageToDom(newMessage());
+    document.querySelector("#count").innerHTML;
+    var unreadMessagesCounter = document.querySelectorAll(".unread").length;
+    document.querySelector("#count").innerHTML = `(${unreadMessagesCounter})`;
+    document.title = "You Have A New Message";
+    alert(document.title);
+  }
 };
 
 
--- D2_DOM_exercises/06-Form-Validation/lib/validation.js
+++ D2_DOM_exercises/06-Form-Validation/lib/validation.js
@@ -1,63 +1,111 @@
-// TODO: Validate this form
-(function() {
-'use strict';
-window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
-var validation = Array.prototype.filter.call(forms, function(form) {
-form.addEventListener('submit', function(event) {
-if (form.checkValidity() === false) {
-event.preventDefault();
-event.stopPropagation();
-}
-form.classList.add('was-validated');
-}, false);
-});
-}, false);
-})();
+// // TODO: Validate this form
+// (function() {
+// 'use strict';
+// window.addEventListener('load', function() {
+// // Fetch all the forms we want to apply custom Bootstrap validation styles to
+// var forms = document.getElementsByClassName('needs-validation');
+// // Loop over them and prevent submission
+// var validation = Array.prototype.filter.call(forms, function(form) {
+// form.addEventListener('submit', function(event) {
+// if (form.checkValidity() === false) {
+// event.preventDefault();
+// event.stopPropagation();
+// }
+// form.classList.add('was-validated');
+// }, false);
+// });
+// }, false);
+// })();
+var form = document.querySelector(".needs-validation");
+var firstNameInput = document.querySelector("#first_name");
+var lastNameInput = document.querySelector("#last_name");
+var addressInput = document.querySelector("#address");
+var countryInput = document.querySelector("#country");
+var zipCodeInput = document.querySelector("#zip_code");
+var cityInput = document.querySelector("#city");
+var emailInput = document.querySelector("#email");
+var phoneInput = document.querySelector("#mobile_phone");
+var checkboxInput = document.querySelector("#tos");
+// firstNameInput.required = true;
+// lastNameInput.required = true;
+// addressInput.required = true;
+// countryInput.required = true;
+// zipCodeInput.required = true;
+// cityInput.required = true;
+// emailInput.required = true;
+// phoneInput.required = true;
+// checkboxInput.checked = true;
+// if (firstNameInput.required != true || lastNameInput.required != true || addressInput.required != true || countryInput.required != true || zipCodeInput.required != true || cityInput.required != true || emailInput.required != true || phoneInput.required != true){
+//     return false;
+// }
+firstNameInput.addEventListener("blur", validateFirstName);
+lastNameInput.addEventListener("blur", validateLastName);
+addressInput.addEventListener("blur", validateAddress);
+countryInput.addEventListener("blur", validateCountry);
+zipCodeInput.addEventListener("blur", validateZipCode);
+cityInput.addEventListener("blur", validateCity);
+emailInput.addEventListener("blur", validateEmail);
+phoneInput.addEventListener("blur", validatePhone);
+form.addEventListener("blur", validation);
 
-function validation(){
-    var firstNameInput = document.querySelector("#first_name");
-    var lastNameInput = document.querySelector("#last_name");
-    var addressInput = document.querySelector("#address");
-    var countryInput = document.querySelector("#country");
-    var zipCodeInput = document.querySelector("#zip_code");
-    var cityInput = document.querySelector("#city");
-    var emailInput = document.querySelector("#email");
-    var phoneInput = document.querySelector("#mobile_phone");
-    var checkboxInput = document.querySelector("#tos");
-    // firstNameInput.required = true;
-    // lastNameInput.required = true;
-    // addressInput.required = true;
-    // countryInput.required = true;
-    // zipCodeInput.required = true;
-    // cityInput.required = true;
-    // emailInput.required = true;
-    // phoneInput.required = true;
-    // checkboxInput.checked = true;
-    // if (firstNameInput.required != true || lastNameInput.required != true || addressInput.required != true || countryInput.required != true || zipCodeInput.required != true || cityInput.required != true || emailInput.required != true || phoneInput.required != true){
-    //     return false;
-    // }
-    if (checkboxInput.checked != true){
+function validateFirstName(){
+    if (firstNameInput.value === ""){
+        // alert("This filed should be filled.");
         return false;
     }
-    if (firstNameInput.value === "" || lastNameInput.value === "" || addressInput.value === "" || countryInput.value === "" || cityInput.value === ""){
+}
+function validateLastName(){
+    if (lastNameInput.value === ""){
+        // alert("This filed should be filled.");
         return false;
     }
+}
+function validateAddress(){
+    if (addressInput.value === ""){
+        // alert("This filed should be filled.");
+        return false;
+    }
+}
+function validateCountry(){
+    if (countryInput.value === ""){
+        // alert("This filed should be filled.");
+        return false;
+    }
+}
+function validateCity(){
+    if (cityInput.value === ""){
+        // alert("This filed should be filled.");
+        return false;
+    }
+}
+function validateEmail(){
+    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailInput.value) != true){
+        // alert("The email is not valid");
+        return false;
+    }
+}
+function validateZipCode(){
     if (/^(?:(?:(?:0[1-9]|[1-8]\d|9[0-4])(?:\d{3})?)|97[1-8]|98[4-9]|‌​‌​2[abAB])$/.test(`${zipCodeInput.value}`) === false){
+        // alert("zip code does not belong to France");
         return false
     }
-    if (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(emailInput.value) != true){
-        return false;
-    }
+}
+function validatePhone(){
     if (/^((\+)33|0)[1-9](\d{2}){4}$/.test(phoneInput.value) != true){
+        // alert("This is not a vild french mobile number");
         return false;
     }
-    return true;
+}
+function validateCheckBox(){
+    if (checkboxInput.checked != true){
+        // alert("To be able to submit the form you need to tick the checkbox");
+        return false;
+    }
 }
 
-if (validation()){
-  var button = document.querySelector(".btn-primary");
-  button.disabled = false;
+function validation(){
+    if(validateFirstName() != false && validateLastName() != false && validateAddress() != false &&validateCountry() != false && validateCity() != false && validateEmail() != false && validateZipCode() != false && validatePhone() != false && validateCheckBox() != false ){
+        var button = document.querySelector(".btn-primary");
+        button.disabled = false;
+    }
 }
